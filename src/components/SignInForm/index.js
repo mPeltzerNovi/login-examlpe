74a45afd0c9ffcb0635f757676fd1a2604cc7939
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-01-17 17:10:48 +0100

    Eind commit van het voordoen.

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -3,17 +3,20 @@ import './SignInForm.css'
 import { useForm } from "react-hook-form";
 
 export default function SignInForm() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, errors } = useForm();
 
     function onSubmit(data) {
         console.log(data);
     }
 
+    //console.log("ERRORS:", errors)
     return <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="userName">UserName</label>
-        <input name="userName" type="text" ref={register}/>
+        <input name="userName" type="text" ref={register({required: true})}/>
+        {errors.userName && <span>This field is required</span>}
         <label htmlFor="password">Password</label>
-        <input name="password" type="password" ref={register}/>
+        <input name="password" type="password" ref={register({required: true})}/>
+        {errors.password && <span>This field is required</span>}
         <input type="submit"/>
     </form>
 }
