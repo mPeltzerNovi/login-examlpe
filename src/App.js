ed5fb89049accac6c4ba0c33b5d4ce190d61b9c5
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-01-17 13:20:54 +0100

    New Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Home from './pages/Home'
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Pofile";
 
 export default function App() {
   return (
@@ -16,10 +20,13 @@ export default function App() {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/about">About</Link>
+                <Link to="/signup">Sign up</Link>
               </li>
               <li>
-                <Link to="/users">Users</Link>
+                <Link to="/signin">Sign in</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
               </li>
             </ul>
           </nav>
@@ -27,15 +34,20 @@ export default function App() {
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
-            <Route path="/about">
-              <About />
+            <Route exact path="/">
+              <Home />
             </Route>
-            <Route path="/users">
-              <Users />
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/profile">
+              <Profile />
             </Route>
             <Route path="/">
               <h1>404 not found, sufferd</h1>
-              <Home />
             </Route>
           </Switch>
         </div>
@@ -43,17 +55,8 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
 
-function Users() {
-  return <h2>Users</h2>;
 
-}
 
 
