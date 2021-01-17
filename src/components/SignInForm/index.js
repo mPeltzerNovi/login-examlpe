d74b78ec298bb1744e4a01563dcef41d002c8472
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-01-17 16:39:49 +0100

    Register inputfields handleSubmit signup form

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './SignInForm.css'
-
+import { useForm } from "react-hook-form";
 
 export default function SignInForm() {
-    return <form>
+    const { register, handleSubmit } = useForm();
+
+    function onSubmit(data) {
+        console.log(data);
+    }
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="userName">UserName</label>
-        <input name="userName" type="text"/>
+        <input name="userName" type="text" ref={register}/>
         <label htmlFor="password">Password</label>
-        <input name="password" type="password"/>
+        <input name="password" type="password" ref={register}/>
         <input type="submit"/>
     </form>
 }
